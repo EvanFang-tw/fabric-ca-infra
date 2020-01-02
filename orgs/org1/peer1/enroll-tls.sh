3da8d257ee153fb6d86d54dd4d5a4aa956db75db
evanfang <evanfang@moneysq.com>
2020-01-02 17:41:32 +0800

    Rename tls certs while enrolling.

--- orgs/org1/peer1/enroll-tls.sh
+++ orgs/org1/peer1/enroll-tls.sh
@@ -15,11 +15,23 @@ docker run \
 --rm \
 --network $NETWORK \
 --env FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/fabric-ca/peer1 \
---env FABRIC_CA_CLIENT_MSPDIR=tls-msp \
+--env FABRIC_CA_CLIENT_MSPDIR=tls \
 --env FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/fabric-ca/tls-ca/ca-cert.pem \
 -v $(pwd)/crypto:/tmp/hyperledger/fabric-ca \
 hyperledger/fabric-ca:1.4.4 \
 /bin/bash -c \
-"fabric-ca-client enroll -d -u https://peer1-org1:peer1PW@ca-tls:7054; chmod -R 777 /tmp/hyperledger/fabric-ca/peer1/tls-msp"
+"fabric-ca-client enroll -d -u https://peer1-org1:peer1PW@ca-tls:7054; chmod -R 777 /tmp/hyperledger/fabric-ca/peer1/tls"
+
+# Rename key
+KEY_DIR=crypto/peer1/tls/keystore
+mv -f $KEY_DIR/*_sk $KEY_DIR/tls.key
+
+# Rename cert
+CERT_DIR=crypto/peer1/tls/signcerts
+mv -f $CERT_DIR/*.pem $CERT_DIR/tls.crt
+
+# Rename tls ca cert
+CACERT_DIR=crypto/peer1/tls/cacerts
+mv -f $CACERT_DIR/*.pem $CACERT_DIR/ca-tls.pem
 
 popd

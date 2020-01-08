7a16abeb00d96dc9e2dacfd9c738f76b4adc5a01
evanfang <evanfang@moneysq.com>
2020-01-08 11:00:40 +0800

    Enable NodeOUs

--- ca/org1/scripts/enroll-admin.sh
+++ ca/org1/scripts/enroll-admin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker exec rca-org1 \
-fabric-ca-client enroll -d -u https://rca-org1-admin:rca-org1-adminpw@rca-org1:7054
+/bin/bash -c \
+"fabric-ca-client enroll -d -u https://rca-org1-admin:rca-org1-adminpw@rca-org1:7054; chmod -R 777 /tmp/hyperledger/fabric-ca/admin"
--- ca/org1/scripts/register.sh
+++ ca/org1/scripts/register.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 docker exec rca-org1 \
-fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW --id.type peer -u https://rca-org1:7054
+fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW \
+--id.affiliation org1 --id.type peer -u https://rca-org1:7054
 
 docker exec rca-org1 \
-fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type peer -u https://rca-org1:7054
+fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW \
+--id.affiliation org1 --id.type peer -u https://rca-org1:7054
 
 docker exec rca-org1 \
-fabric-ca-client register -d --id.name admin-org1 --id.secret org1AdminPW --id.type admin -u https://rca-org1:7054
+fabric-ca-client register -d --id.name admin-org1 --id.secret org1AdminPW \
+--id.affiliation org1 --id.type admin -u https://rca-org1:7054
 
 docker exec rca-org1 \
-fabric-ca-client register -d --id.name user-org1 --id.secret org1UserPW --id.type user -u https://rca-org1:7054
+fabric-ca-client register -d --id.name user-org1 --id.secret org1UserPW  \
+--id.affiliation org1 --id.type user -u https://rca-org1:7054
